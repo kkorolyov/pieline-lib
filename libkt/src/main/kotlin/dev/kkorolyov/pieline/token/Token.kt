4d8b06246a1c7e3d51b4c6a875c41cfc70317d22
kkorolyov <kirillk753@gmail.com>
2020-12-10 23:47:03 -0800

    Encapsulate token interception

--- libkt/src/main/kotlin/dev/kkorolyov/pieline/token/Token.kt
+++ libkt/src/main/kotlin/dev/kkorolyov/pieline/token/Token.kt
@@ -1,28 +1,43 @@
 package dev.kkorolyov.pieline.token
 
 import io.grpc.Context
+import io.grpc.Contexts
 import io.grpc.Metadata
+import io.grpc.Metadata.Key
+import io.grpc.ServerCall
+import io.grpc.ServerCall.Listener
+import io.grpc.ServerCallHandler
+import io.grpc.ServerInterceptor
+
+/**
+ * This context's authorization token.
+ */
+val Context.token: String?
+	get() = TOKEN_KEY.get(this)
+
+/**
+ * Server interceptor which applies incoming header authorization token to the current [Context].
+ * Services have access to this token through [Context.token].
+ */
+val TOKEN_INTERCEPTOR = object : ServerInterceptor {
+	override fun <ReqT : Any?, RespT : Any?> interceptCall(
+		call: ServerCall<ReqT, RespT>,
+		headers: Metadata,
+		next: ServerCallHandler<ReqT, RespT>
+	): Listener<ReqT> {
+		return Contexts.interceptCall(
+			Context.current().withValue(
+				TOKEN_KEY,
+				headers.get(Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER))?.removePrefix("Bearer ")
+			),
+			call,
+			headers,
+			next
+		)
+	}
+}
 
 /**
  * Internal context key for storing tokens.
  */
-private val tokenKey = Context.key<String>("token")
-
-/**
- * Returns a new context with access [token].
- */
-fun Context.withToken(token: String?): Context = token?.let {
-	withValue(tokenKey, it)
-} ?: this
-
-/**
- * This context's access token.
- */
-val Context.token: String?
-	get() = tokenKey.get(this)
-
-/**
- * `Authorization` header's access token.
- */
-val Metadata.token: String?
-	get() = get(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER))?.removePrefix("Bearer ")
+private val TOKEN_KEY = Context.key<String>("token")
